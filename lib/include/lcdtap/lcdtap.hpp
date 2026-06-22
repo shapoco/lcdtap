312c6584688018ce8cf23cbcde9aa64d13d6d0f9
shapoco <40152459+shapoco@users.noreply.github.com>
2026-06-22 12:39:35 +0900

    fix

--- lib/include/lcdtap/lcdtap.hpp
+++ lib/include/lcdtap/lcdtap.hpp
@@ -183,7 +183,7 @@ enum class ConfigPreset : uint8_t {
 static const char* CONFIG_PRESET_NAMES[] = {
     "ILI9341",    "ILI9342", "ILI9488",    "SSD1306",        "SSD1331",
     "ST7735",     "ST7789",  "Arduboy",    "M5Stack CoreS3", "PicoPad",
-    "Picosystem", "Thumby",  "TinyJoypad", "Wio Terminal",   "Xiamocon",
+    "PicoSystem", "Thumby",  "TinyJoypad", "Wio Terminal",   "Xiamocon",
 };
 static_assert(sizeof(CONFIG_PRESET_NAMES) / sizeof(CONFIG_PRESET_NAMES[0]) ==
                   static_cast<size_t>(ConfigPreset::NUM_PRESETS),
