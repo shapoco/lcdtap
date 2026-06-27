55122eb911187e307630ac71de3bc5198d776489
shapoco <40152459+shapoco@users.noreply.github.com>
2026-06-27 10:59:12 +0900

    fix PCB

--- example/pico2_universal/kicad/panelize/panelize.bat
+++ example/pico2_universal/kicad/panelize/panelize.bat
@@ -1,12 +1,11 @@
-rem Rev.1
+rem Rev.2
 
 set PROJ_NAME=lcdtap_rp2350
-set REPO_DIR=%WSL_HOME_DIR%\repo\2026\lcdtap
-copy %REPO_DIR%\kicad\rp2350\%PROJ_NAME%.kicad_pcb
+copy %WSL_LCDTAP_DIR%\example\pico2_universal\kicad\%PROJ_NAME%.kicad_pcb
 
 %KICAD10_SCRIPTS_PATH%\kikit.exe panelize ^
 	--layout "grid; rows: 1; cols: 3; space: 2mm" ^
-	--tabs "fixed; width: 5mm; hcount: 3; vcount: 2;" ^
+	--tabs "fixed; width: 5mm; hcount: 1; vcount: 2;" ^
 	--cuts "mousebites; drill: 0.6mm; spacing: 1.0mm; offset: 0mm" ^
 	--framing "railstb; width: 5mm; space: 2mm;" ^
 	--post "millradius: 1mm" ^
@@ -15,4 +14,4 @@ copy %REPO_DIR%\kicad\rp2350\%PROJ_NAME%.kicad_pcb
 	%PROJ_NAME%.kicad_pcb ^
 	%PROJ_NAME%_panelized.kicad_pcb
 
-copy %PROJ_NAME%_panelized.kicad_pcb %REPO_DIR%\kicad\rp2350\panelize\%PROJ_NAME%_panelized.kicad_pcb
+copy %PROJ_NAME%_panelized.kicad_pcb %WSL_LCDTAP_DIR%\example\pico2_universal\panelize\%PROJ_NAME%_panelized.kicad_pcb
